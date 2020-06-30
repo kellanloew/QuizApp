6eb29835ca282ce00916146e4feae9365ab370d8
unknown <kellanloew@gmail.com>
2020-06-29 21:48:04 -0600

    renamed scripts

---
+++ db/db_create_questions_table.sql
@@ -0,0 +1,26 @@
+USE [quiz_db]
+GO
+
+/****** Object:  Table [dbo].[question]    Script Date: 6/29/2020 6:43:12 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[question]') AND type in (N'U'))
+DROP TABLE [dbo].[question]
+GO
+
+/****** Object:  Table [dbo].[question]    Script Date: 6/29/2020 6:43:12 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[question](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[QuestionText] nvarchar(1000) NOT NULL,
+	[IsComplete] [bit] NOT NULL,
+ CONSTRAINT [PK_question] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
